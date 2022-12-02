ff96a5cf64eb58dfb90bd8f92efd872b374804da
suyog ojha <ojhasuyog2020@gmail.com>
2022-12-02 16:23:36 +0545

    url Added

--- bookapp/admin.py
+++ bookapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    prepopulated_fields = {'slug': ('name',)}
+
+
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    
+admin.site.register(Book, BookAdmin)
--- booksite/urls.py
+++ booksite/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('book/', include('bookapp.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
