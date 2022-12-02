9ea13c270ea36503db5478bd212b00ea76ec4ce7
suyog ojha <ojhasuyog2020@gmail.com>
2022-12-02 15:59:11 +0545

    !!

---
+++ bookapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BookappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bookapp'
---
+++ bookapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField('Categories', max_length=50)
+    slug = models.SlugField(max_length = 50)
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length = 100)
+    slug = models.SlugField(max_length=100)
+    cover_image = models.ImageField(upload_to = 'img', blank = True, null = True)
+    author = models.CharField(max_length=50)
+    summary = models.TextField()
+    category = models.ManyToManyField(Category, related_name='books')
+    pdf = models.FileField(upload_to='pdf')
+    recommended_books = models.BooleanField(default=False)
+    fiction_books = models.BooleanField(default=False)
+    business_books = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
