88d321b45d960f0df8e6f69bb58cedc6c4cb115f
suyog ojha <ojhasuyog2020@gmail.com>
2022-12-03 11:18:34 +0545

    admin

--- bookapp/urls.py
+++ bookapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('homee', views.home, name = 'homee'),
+    path('home', views.home, name = 'home'),
     path('all_books', views.all_books, name = 'all_books'),
     path('genre/<str:slug>', views.category_detail, name = 'category_detail'),
     path('pdf/<str:slug>', views.book_detail, name = 'book_detail'),
